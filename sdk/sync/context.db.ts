f1d44a24ce477f26a96865c96550e42721631fe7
andrey <andrey-ne@mail.ru>
2024-03-21 20:14:26 +0100

    fix

---
+++ sdk/sync/context.db.ts
@@ -0,0 +1,9 @@
+import { IndexedDatabase } from './indexedDatabase'
+
+class ContextDb extends IndexedDatabase<Uint8Array>{
+  constructor () {
+    super("contexts")
+  }
+}
+
+export const contextDB = new ContextDb();
